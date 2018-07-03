a6c649801f2d59e90f4d5bebac521db8703f28b0
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-03 03:19:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
+def countdown_with_sleep
 
-def countdown
+end
+
+def countdown(number)
+  while number > 0
+    puts "#{countdown-to-midnight} SECOND(S)!"
+    s
+  end
+  return "HAPPY NEW YEAR!"
 end
